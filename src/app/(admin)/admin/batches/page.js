f1e17c345b89cdc00679f72c6e6324fce2361c9c
MunawarKhan1206 <munawar.kse0321@gmail.com>
2024-11-24 19:07:13 +0500

    DataTables and Dialogs Added

--- src/app/(admin)/admin/batches/page.js
+++ src/app/(admin)/admin/batches/page.js
@@ -1,7 +1,14 @@
+import { BatchesTable } from "@/components/DataTables/BatchTable";
+import { BatchModal } from "@/components/Dialogs/BatchModal";
+
 export default function Batches() {
     return (
         <div className="min-h-screen p-10">
-            <h1 className="text-3xl font-bold text-center">Batches</h1>
-        </div>
+      <div className="flex justify-between my-2">
+        <h1 className="text-3xl font-bold text-center">Courses</h1>
+ <BatchModal />      
+      </div>
+      <BatchesTable />
+    </div>
     );
 }
--- src/app/(admin)/admin/courses/page.js
+++ src/app/(admin)/admin/courses/page.js
@@ -1,7 +1,14 @@
+import { CourseTable } from "@/components/DataTables/CourseTable";
+import { CourseDialog } from "@/components/Dialogs/CourseModal";
+
 export default function Courses() {
-    return (
-            <div className="min-h-screen p-10">
-                <h1 className="text-3xl font-bold text-center">Courses</h1>
-            </div>
-        );
-    }
+  return (
+    <div className="min-h-screen p-10">
+      <div className="flex justify-between my-2">
+        <h1 className="text-3xl font-bold text-center">Courses</h1>
+  <CourseDialog />      
+      </div>
+      <CourseTable />
+    </div>
+  );
+}
